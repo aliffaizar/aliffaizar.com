00302fe84f656317d75a7d089b4dc22dbc6fadef
Alif Faizar <aliffaizar21@gmail.com>
2022-09-03 21:46:24 +0700

    test action

---
+++ src/config.mjs
@@ -0,0 +1,11 @@
+export const SITE_TITLE = 'Alif Faizar';
+export const HOME = {
+  title: 'Alif Faizar',
+  description: `I'm alif faizar a web developer`,
+  heroTitle: `Hi, I'm Alif`,
+  heroTagline: `I'm a web developer based in Tasikmalya, Indonesia`,
+  summary: {
+    title: '',
+    about: '',
+  },
+};
