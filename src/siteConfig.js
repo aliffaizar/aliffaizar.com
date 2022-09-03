a292df333e689bfc5945c102975bf3acdcf7d25f
Alif Faizar <aliffaizar21@gmail.com>
2022-09-03 22:15:29 +0700

    fix error

---
+++ src/siteConfig.js
@@ -0,0 +1,14 @@
+const siteConfig = {
+  SITE_TITLE: 'Alif Faizar',
+  HOME: {
+    title: 'Alif Faizar',
+    description: `I'm alif faizar a web developer`,
+    heroTitle: `Hi, I'm Alif`,
+    heroTagline: `I'm a web developer based in Tasikmalya, Indonesia`,
+    summary: {
+      title: '',
+      about: '',
+    },
+  },
+};
+export default siteConfig;
